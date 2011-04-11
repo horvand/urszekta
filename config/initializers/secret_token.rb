d60c1bb9df30caccd809f3f88b3592cd48fb7e31
horvand <horvand@gmail.com>
2011-04-11 09:15:25 +0100

    started with rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urszekta::Application.config.secret_token = '5992798e0192b1f4337952f67d978fc2bcd381de7ce8c09cb2280d51e8fd171cc21cdaf30e25e0c5b95c479fda3a700a18fa4d12ca425af717f0d9fa39725172'
